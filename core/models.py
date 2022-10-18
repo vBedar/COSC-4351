ab5d3506a2eead1e0dbe23161171073521420630
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2022-10-18 11:19:27 -0500

    make signin and signup

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from distutils.command.upload import upload
+from email.policy import default
+from pyexpat import model
+from statistics import mode
+from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+# Create your models here.
+User = get_user_model()
+
+# Create your models here.
