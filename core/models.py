946a09fd02c490a5ff6330f1c4b59eee7a2fa893
DylanL826 <dylanl826@gmail.com>
2022-11-28 15:28:27 -0600

    Created list High_Traffic_Days list in models file.

--- core/models.py
+++ core/models.py
@@ -104,4 +104,22 @@ class HighTrafficDay(models.Model):
     #MLK - 3rd Monday of January
     #Halloween - 10/31
 
+# List of name, date pairs of high traffic days.
+High_Traffic_Days = [
+    ('Christmas', datetime(2021, 12, 25)),
+    ('Christmas Eve', datetime(2021, 12, 24)),
+    ('Thanksgiving', datetime(2021, 11, 25)),
+    ('Good Friday', datetime(2021, 4, 2)),
+    ('Labor Day', datetime(2021, 9, 6)),
+    ('New Years Day', datetime(2021, 1, 1)),
+    ('New Years Eve', datetime(2021, 12, 31)),
+    ('4th of July', datetime(2021, 7, 4)),
+    ('Memorial Day', datetime(2021, 5, 31)),
+    ('Veterans Day', datetime(2021, 11, 11)),
+    ('MLK', datetime(2021, 1, 18)),
+    ('Halloween', datetime(2021, 10, 31)),
+]
+
+
+
 
